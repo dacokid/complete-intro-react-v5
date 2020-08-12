e421db975b5b1d0b22671ab4d492dc283939abe4
Daniel Cunha <danielcc.jp@gmail.com>
2020-08-12 11:35:25 -0300

    + .gitignore, eslint, prettier

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 // throw new Error("lol")
 const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed),
   ]);
 };
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
+  return React.createElement('div', { id: 'something-important' }, [
+    React.createElement('h1', {}, 'Adopt Me!'),
     React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
+      name: 'Luna',
+      animal: 'Dog',
+      breed: 'Havanese',
     }),
     React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
+      name: 'Pepper',
+      animal: 'Bird',
+      breed: 'Cockatiel',
     }),
     React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed",
+      name: 'Doink',
+      animal: 'Cat',
+      breed: 'Mixed',
     }),
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
